d958219e417b8c70e85f5c2fa78ebbc4001cbab4
Developer <developer@it.step>
2018-11-02 21:13:10 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>For The HORDE</title>
+    <style>
+        body {
+            background-image: url(image/HORDE.jpg); /* Путь к фоновому изображению */
+            background-color: #c7b39b; /* Цвет фона */
+        }
+    </style>
+</head>
+<header>
+
+    <h1>FOR THE HORDE
+
+    </h1>
+
+</header>
+<body>
+
+<h2>
+
+<p>
+    FORGE DE HAS MI PRINTY LOKAR
+</p>
+</h2>
+
+</body>
+<main>
+    <br>
+
+    <br>
+
+    <?php
+    //<?=" For The HORDE";? >// приравнивается надписи ниже
+    echo "Lok'tar ogar, ";
+
+
+    print("   My life belongs to the horde");
+
+    ?>
+    <h1>
+        <?php
+        $_POSRT =[];
+        $title ="Death to all living things";
+        $a=1;
+        $b=2;
+        $sum ="";
+        $name='';
+
+
+
+?>
+</main>
+<p>Day of the DEAD alince</p>
+</html><?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 02.11.2018
+ * Time: 18:58
+ */
